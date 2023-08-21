e733d5e3fa3334cc8a86b61fd2e2c1a55d6f381c
Sulthon <sultona412@gmail.com>
2023-08-21 18:44:43 +0700

    modified

--- src/components/sliderHome.jsx
+++ src/components/sliderHome.jsx
@@ -29,11 +29,13 @@ export default function SliderHome({ items }) {
                                 height="0"
                                 sizes="100%"
                                 className="w-full h-full object-cover object-top hover:scale-105 transition" loading={'eager'} />
-                            <div className="absolute left-0 right-0 bottom-0 px-5 py-8 text-white bg-slate-950/60">
+                            <div className="absolute left-0 right-0 bottom-0 px-5 py-5 text-white flex justify-between items-center w-full gap-x-5 whitespace-nowrap bg-sky-950/60 backdrop-blur-sm">
                                 <div>
-                                    <h1 className="rounded md:text-4xl mb-2 font-semibold">{title}</h1>
-                                    <p className="text-sm mb-5">{release_date} - {vote_average}</p>
-                                    <Link href={"/"} className={"bg-rose-700/95 px-7 py-3 rounded-lg text-slate-50 shadow-lg"}>Watch</Link>
+                                    <h1 className="rounded md:text-4xl mb-2 font-semibold whitespace-normal">{title}</h1>
+                                    <p className="text-sm">{release_date}</p>
+                                </div>
+                                <div>
+                                    <Link href={"/"} className={"bg-rose-700/95 px-7 py-3 rounded-lg text-slate-50 shadow-lg active:bg-rose-500 hover:shadow-md hover:shadow-sky-300/50"}>Watch now</Link>
                                 </div>
                             </div>
                         </div>
