6dfa3e8303368949bde8117fc7abd3e76039de75
Sulthon <sultona412@gmail.com>
2023-08-24 21:29:20 +0700

    refactoring card popular movies

--- src/components/cardMovie.jsx
+++ src/components/cardMovie.jsx
@@ -1,19 +1,31 @@
 import { BookmarkIcon } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 export default function CardMovie() {
   return (
     <article className="overflow-hidden rounded-lg border-2 border-slate-50/50">
-      <img
-        src="https://image.tmdb.org/t/p/original/jZIYaISP3GBSrVOPfrp98AMa8Ng.jpg"
-        alt=""
-      />
-      <div className="bg-slate-50/30 p-2 backdrop-blur">
-        <h1>Spiderman No Way Home</h1>
-        <div className="flex justify-between gap-x-5">
-          <button className="">Play</button>
-          <div className="rounded-full border border-slate-500/90 p-2">
-            <BookmarkIcon className="h-4 w-4 " />
-          </div>
+      <div className="relative h-52">
+        <Image
+          src="https://image.tmdb.org/t/p/original/jZIYaISP3GBSrVOPfrp98AMa8Ng.jpg"
+          alt=""
+          fill
+          objectFit="cover"
+        />
+      </div>
+      <div className="bg-slate-50/30 px-4 py-4 backdrop-blur">
+        <h1 className="text-xl">Spiderman No Way Home</h1>
+        <div className="flex items-center gap-x-2">
+          <StarIcon className="h-4 w-4" />
+          <p className="text-sm opacity-80">9 rating</p>
+        </div>
+        <div className="mt-5 flex justify-between gap-x-5">
+          <button className="rounded border border-transparent bg-red-800 px-5 py-2 shadow hover:border-red-800 hover:bg-transparent active:bg-red-500">
+            Play
+          </button>
+          <button className="flex place-items-center rounded-full border border-slate-300/90 px-3 py-2 hover:bg-white/40 [&>svg]:hover:fill-sky-900">
+            <BookmarkIcon className="h-4 w-4" />
+          </button>
         </div>
       </div>
     </article>
--- src/components/popularMovie.jsx
+++ src/components/popularMovie.jsx
@@ -1,17 +1,21 @@
+import { StarIcon } from "@heroicons/react/24/solid";
 import CardMovie from "./cardMovie";
 
 export default function PopularMovie() {
-    const array = Array.from({ length: 5 })
-    return (
-        <section className="container">
-            <h1 className="text-xl font-medium my-5">Popular Movies</h1>
-            <div className="grid md:grid-cols-12 gap-5">
-                {array.map((_, index) =>
-                (<div className="col-span-3" key={index}>
-                    <CardMovie />
-                </div>)
-                )}
-            </div>
-        </section>
-    )
-};
+  const array = Array.from({ length: 5 });
+  return (
+    <section className="container my-14">
+      <div className="my-5 flex items-center gap-x-5">
+        <h1 className="text-2xl font-medium">Popular Movies</h1>
+        <StarIcon className="h-6 w-6 fill-yellow-300" />
+      </div>
+      <div className="grid gap-5 md:grid-cols-12">
+        {array.map((_, index) => (
+          <div className="col-span-3" key={index}>
+            <CardMovie />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
