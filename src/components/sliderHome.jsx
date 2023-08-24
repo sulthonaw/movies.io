2619487786d731c00a2ff81cc883959668152aec
Sulthon <sultona412@gmail.com>
2023-08-24 21:19:05 +0700

    add parent in element image

--- src/components/sliderHome.jsx
+++ src/components/sliderHome.jsx
@@ -1,47 +1,64 @@
-'use client'
+"use client";
 
-import Slider from "react-slick"
+import Slider from "react-slick";
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 import Link from "next/link";
 
-
-
 export default function SliderHome({ items }) {
-    let settings = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        autoplay: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
+  let settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-    return (
-        <section>
-            <Slider {...settings}>
-                {items.map(({ id, backdrop_path, title, release_date }) => {
-                    return (
-                        <div key={id} className="bg-slate-50 w-full h-[30vh] md:h-[50vh] text-slate-900 relative overflow-hidden">
-                            <Image src={"https://image.tmdb.org/t/p/original" + backdrop_path} alt={title} width="0"
-                                height="0"
-                                sizes="100%"
-                                className="w-full h-full object-cover object-top hover:scale-105 transition" loading={'eager'} />
-                            <div className="absolute left-0 right-0 bottom-0 px-5 py-5 text-white flex justify-between items-center w-full gap-x-5 whitespace-nowrap bg-sky-950/60 backdrop-blur-sm">
-                                <div>
-                                    <h1 className="rounded md:text-4xl mb-2 font-semibold whitespace-normal">{title}</h1>
-                                    <p className="text-sm">{release_date}</p>
-                                </div>
-                                <div>
-                                    <Link href={"/"} className={"bg-rose-700/95 px-7 py-3 rounded-lg text-slate-50 shadow-lg active:bg-rose-500 hover:shadow-md hover:shadow-sky-300/50"}>Watch now</Link>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
-            </Slider>
-        </section >
-    )
-};
+  return (
+    <section>
+      <Slider {...settings}>
+        {items.map(({ id, backdrop_path, title, release_date }) => {
+          return (
+            <div
+              key={id}
+              className="relative h-[30vh] w-full overflow-hidden bg-slate-50 text-slate-900 md:h-[50vh]"
+            >
+              <div className="relative">
+                <Image
+                  src={"https://image.tmdb.org/t/p/original" + backdrop_path}
+                  alt={title}
+                  width="0"
+                  height="0"
+                  sizes="100%"
+                  className="h-full w-full object-cover object-top transition hover:scale-105"
+                  loading={"eager"}
+                />
+              </div>
+              <div className="absolute bottom-0 left-0 right-0 flex w-full items-center justify-between gap-x-5 whitespace-nowrap bg-sky-950/60 px-5 py-5 text-white backdrop-blur-sm">
+                <div>
+                  <h1 className="mb-2 whitespace-normal rounded font-semibold md:text-4xl">
+                    {title}
+                  </h1>
+                  <p className="text-sm">{release_date}</p>
+                </div>
+                <div>
+                  <Link
+                    href={"/"}
+                    className={
+                      "rounded-lg bg-rose-700/95 px-7 py-3 text-slate-50 shadow-lg hover:shadow-md hover:shadow-sky-300/50 active:bg-rose-500"
+                    }
+                  >
+                    Watch now
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </Slider>
+    </section>
+  );
+}
