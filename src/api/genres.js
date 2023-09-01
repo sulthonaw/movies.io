38f28e4b30a4e014de08a94012255879d5ced12e
Sulthon <sultona412@gmail.com>
2023-09-01 22:19:18 +0700

    add genre

---
+++ src/api/genres.js
@@ -0,0 +1,19 @@
+async function getGenres() {
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_TMDB_API_BASE_URL + "/genre/movie/list",
+    {
+      headers: {
+        Authorization:
+          "Bearer " + process.env.NEXT_PUBLIC_TMDB_READ_ACCESS_TOKEN,
+      },
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error("Error fetch");
+  }
+
+  return res.json();
+}
+
+export { getGenres };
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,36 +4,35 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Nav() {
-  const [toggle, setToggle] = useState('hidden', []);
-  const [header, setHeader] = useState(false)
+  const [toggle, setToggle] = useState("hidden", []);
+  const [header, setHeader] = useState(false);
 
   const listenScrollEvent = (event) => {
     if (window.scrollY < 73) {
-      return setHeader(false)
-    } return setHeader(true)
-  }
+      return setHeader(false);
+    }
+    return setHeader(true);
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', listenScrollEvent);
-    return () =>
-      window.removeEventListener('scroll', listenScrollEvent);
+    window.addEventListener("scroll", listenScrollEvent);
+    return () => window.removeEventListener("scroll", listenScrollEvent);
   }, []);
-
 
   const routeName = [
     {
-      title: 'Home',
-      route: "/"
+      title: "Home",
+      route: "/",
     },
     {
-      title: 'Movies',
-      route: "/movies"
+      title: "Movies",
+      route: "/movies",
     },
     {
-      title: 'Genre',
-      route: "/"
-    }
-  ]
+      title: "Genre",
+      route: "/",
+    },
+  ];
 
   return (
     <header className={`sticky top-0 z-50 ${header && "bg-sky-800/90"}`}>
@@ -42,7 +41,7 @@ export default function Nav() {
           Movies.Io
         </Link>
         <button
-          onClick={() => setToggle(toggle == 'hidden' ? 'flex' : 'hidden')}
+          onClick={() => setToggle(toggle == "hidden" ? "flex" : "hidden")}
           className="col-span-1 place-self-end rounded border border-transparent p-2 hover:border-slate-100 active:bg-slate-500 md:hidden"
         >
           <svg
@@ -62,31 +61,36 @@ export default function Nav() {
         </button>
 
         <div
-          className={`col-span-full ${toggle} flex-col gap-y-4 md:gap-x-5 py-3 md:flex md:col-span-10 md:flex-row md:items-center md:justify-between`}
+          className={`col-span-full ${toggle} flex-col gap-y-4 py-3 md:col-span-10 md:flex md:flex-row md:items-center md:justify-between md:gap-x-5`}
         >
           <ul className="flex flex-col items-end gap-y-5 md:flex-row md:gap-x-5">
             {routeName.map(function ({ route, title }) {
               return (
                 <li key={title}>
-                  <Link href={route} className="border-b border-transparent hover:border-sky-200 active:text-sky-300">{title}</Link>
+                  <Link
+                    href={route}
+                    className="border-b border-transparent hover:border-sky-200 active:text-sky-300"
+                  >
+                    {title}
+                  </Link>
                 </li>
-              )
+              );
             })}
           </ul>
-          <div className="flex flex-col items-end md:items-center gap-y-5 md:flex-row md:gap-x-5">
+          <div className="flex flex-col items-end gap-y-5 md:flex-row md:items-center md:gap-x-5">
             <form>
               <input
                 type="search"
                 placeholder="search"
                 name="search"
-                className="rounded-full text-sm bg-slate-50/20 px-5 py-3 placeholder:text-slate-300 focus:outline focus:outline-slate-100"
+                className="rounded-full bg-slate-50/20 px-5 py-3 text-sm placeholder:text-slate-300 focus:outline focus:outline-slate-100"
               />
             </form>
             <div className="flex gap-x-3">
               <button className="rounded-lg px-4 py-2 hover:outline hover:outline-1 active:bg-sky-600">
                 <Link href="/">Sign In</Link>
               </button>
-              <button className="rounded-lg bg-sky-600 hover:bg-sky-800 border border-transparent hover:border-sky-100 active:bg-sky-600 px-4 py-2">
+              <button className="rounded-lg border border-transparent bg-sky-600 px-4 py-2 hover:border-sky-100 hover:bg-sky-800 active:bg-sky-600">
                 <Link href="/">Sign Up</Link>
               </button>
             </div>
